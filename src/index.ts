f10a40fffbf803727f415a0fa703aa4015942a6f
seunghwan11.kim@lgepartner.com-at-509259651798 <seunghwan.kim@freedgrouptech.com>
2025-07-07 16:27:00 +0900

    feat: error handling

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import express from "express";
 import { env } from "./configs/env.config";
-import { validateEnv } from "./utils/validators/env.validator";
-import { AppEnv } from "./types/env.type";
 import { modules } from "./modules";
 import cors from "cors";
-let validatedEnv: AppEnv;
-try {
-  validatedEnv = validateEnv(env);
-  console.log("환경 변수 유효성 검사 성공.");
-} catch (error: any) {
-  console.error("환경 변수 설정 오류:", error.message);
-  process.exit(1);
-}
-
+import { errorMiddleware } from "./middlewares/error.middleware";
 const PORT = env.SERVER.PORT;
 
 const app = express();
@@ -26,6 +16,7 @@ app.use(
 );
 
 modules(app);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
