510988a526fda8ab409528292137623c7cc42803
sh5080 <prpn97@gmail.com>
2025-07-13 15:39:17 +0900

    refactor: directory alias

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Express } from "express";
-import { postRouter } from "./post/post.router";
-import { authRouter } from "./auth/auth.router";
+import { postRouter } from "@post/post.router";
+import { authRouter } from "@auth/auth.router";
 
 export const routes = (app: Express) => {
   app.use("/post", postRouter);
--- src/types/image.type.ts
+++
@@ -1,4 +0,0 @@
-export interface ImageData {
-  data: string;
-  mimeType: string;
-}
