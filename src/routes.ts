cc4dd3993727584dd8329786fb8c6a746e62268e
sh5080 <prpn97@gmail.com>
2025-07-13 16:17:38 +0900

    feat: api prefix

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
-import { Express } from "express";
-import { postRouter } from "@post/post.router";
+import { Express, Router } from "express";
 import { authRouter } from "@auth/auth.router";
+import { postRouter } from "@post/post.router";
+import { userRouter } from "@user/user.router";
 
-export const routes = (app: Express) => {
-  app.use("/post", postRouter);
-  app.use("/auth", authRouter);
+export const routes = (app: Express): void => {
+  const apiRouter: Router = Router();
+
+  apiRouter.use("/auth", authRouter);
+  apiRouter.use("/post", postRouter);
+  apiRouter.use("/user", userRouter);
+
+  app.use("/api", apiRouter);
 };
