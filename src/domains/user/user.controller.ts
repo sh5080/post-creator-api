8aa06fdcc8b2b2358a33ba372a2bd6d0763dce36
sh5080 <prpn97@gmail.com>
2025-07-16 17:49:12 +0900

    feat: 닉네임변경시 auth토큰 재생성

--- src/domains/user/user.controller.ts
+++ src/domains/user/user.controller.ts
@@ -7,12 +7,15 @@ import {
   UpdateNicknameResponseDto,
 } from "./dto/user.dto";
 import { AuthRequest } from "@common/types/request.type";
+import { AuthService } from "@auth/auth.service";
+import { sendAuthTokens } from "@common/utils/token.util";
 
 export class UserController {
   private userService: UserService;
-
+  private authService: AuthService;
   constructor() {
     this.userService = new UserService();
+    this.authService = new AuthService();
   }
 
   signup = async (req: Request, res: Response, next: NextFunction) => {
@@ -39,11 +42,14 @@ export class UserController {
       const dto = updateNicknameValidator.validate(req.body);
       const { userId } = req.user!;
       const user = await this.userService.updateNickname(userId, dto);
-
       const responseDto: UpdateNicknameResponseDto = {
         nickname: user.nickname,
         updatedAt: user.updatedAt,
       };
+      const { accessToken, refreshToken } =
+        await this.authService.generateAuthTokens(user);
+
+      await sendAuthTokens(res, accessToken, refreshToken);
 
       res.status(200).json(responseDto);
     } catch (error) {
