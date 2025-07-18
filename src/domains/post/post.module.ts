813a549dbe2835f9735d5fb4728421d97c672f1a
sh5080 <prpn97@gmail.com>
2025-07-18 17:55:34 +0900

    feat: inject post repository

--- src/domains/post/post.module.ts
+++ src/domains/post/post.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PostService } from "./post.service";
 import { PostController } from "./post.controller";
+import { PostRepository } from "./post.repository";
 
 @Module({
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, PostRepository],
   exports: [PostService],
 })
 export class PostModule {}
